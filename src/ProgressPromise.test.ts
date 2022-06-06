a9620bdfc5a221af07dda07f414d55354df79087
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-06-06 11:25:24 +0300

    Cover `ProgressPromise::then()` with unit tests

--- src/ProgressPromise.test.ts
+++ src/ProgressPromise.test.ts
@@ -1,10 +1,10 @@
 import { ProgressPromise } from './ProgressPromise';
 
-describe('ProgressPromise', () => {
-    it('should resolve static values', () => {
+describe('ProgressPromise::then()', () => {
+    it('should resolve static values', async () => {
         const promise = ProgressPromise.resolve(42);
 
-        promise.then((value) => expect(value).toBe(42));
+        return promise.then((value) => expect(value).toBe(42));
     });
 
     it('should resolve another progress promise to itself', () => {
@@ -13,7 +13,7 @@ describe('ProgressPromise', () => {
         expect(ProgressPromise.resolve(promise)).toBe(promise);
     });
 
-    it('should report progress', () => {
+    it('should report progress', async () => {
         const progressHistory: number[] = [];
 
         const promise = new ProgressPromise((resolve, _reject, progress) => {
@@ -24,10 +24,108 @@ describe('ProgressPromise', () => {
             setTimeout(resolve, 50);
         });
 
-        promise.then(
-          () => expect(progressHistory).toEqual([25, 50, 75, 100]),
-          undefined,
-          (progress) => progressHistory.push(progress),
-        )
+        return promise.then(
+            () => expect(progressHistory).toEqual([25, 50, 75, 100]),
+            undefined,
+            (progress) => progressHistory.push(progress),
+        );
+    });
+
+    it('should properly handle chained returns', async () => {
+        return new ProgressPromise((resolve) => resolve('Hello'))
+            .then((value) => {
+                return `${value} world`;
+            })
+            .then((value) => {
+                return `${value}!`;
+            })
+            .then(
+                (value) => expect(value).toBe('Hello world!'),
+                () => fail('This should not happen'),
+            );
+    });
+
+    it('should properly handle chained returned promises', async () => {
+        return new ProgressPromise((resolve) => resolve('Hello'))
+            .then((value) => {
+                return new Promise((resolve) => resolve(`${value} world`));
+            })
+            .then((value) => {
+                return new Promise((resolve) => resolve(`${value}!`));
+            })
+            .then(
+                (value) => expect(value).toBe('Hello world!'),
+                () => fail('This should not happen'),
+            );
+    });
+
+    it('should properly handle chained errors', async () => {
+        const errors: Error[] = [];
+
+        return new ProgressPromise((_resolve, reject) => reject('Error #0'))
+            .catch((error) => {
+                errors.push(error);
+                throw `Error #1`;
+            })
+            .catch((error) => {
+                errors.push(error);
+                throw `Error #2`;
+            })
+            .then(
+                () => fail('This should not happen'),
+                (error) => {
+                    expect(errors).toEqual(['Error #0', 'Error #1']);
+                    expect(error).toBe('Error #2');
+                },
+            );
+    });
+
+    it('should properly handle caught errors', async () => {
+        return new ProgressPromise((_resolve, reject) => reject('Error'))
+          .catch((error) => {
+              return error;
+          })
+          .then(
+            (value) => expect(value).toBe('Error'),
+            () => fail('This should not happen'),
+          );
+    });
+
+    it('should properly handle chained then/catch calls', async () => {
+        return new ProgressPromise((resolve) => resolve('Hello'))
+          .then(
+            (value) => { throw value },
+            () => fail('This should not happen.'),
+          )
+          .then(
+            () => fail('This should not happen.'),
+            (error) => `${error} world!`,
+          )
+          .then(
+            (value) => expect(value).toBe('Hello world!'),
+            () => fail('This should not happen.'),
+          );
+    });
+
+    it('should properly unwrap nested promises', async () => {
+        return new ProgressPromise<number>((resolve) => resolve(42))
+          .then((value) => {
+              return new ProgressPromise<number>((resolve) => resolve(value + 1))
+                .then((value) => {
+                    return new ProgressPromise<number>((resolve) => resolve(value + 1));
+                })
+          })
+          .then(
+            (value) => expect(value).toBe(44),
+            () => fail('This should not happen'),
+          );
+    });
+
+
+
+    it('should be compatible with async/await code', async () => {
+        const value = await new ProgressPromise<number>((resolve) => resolve(42));
+
+        expect(value).toBe(42);
     });
 });
